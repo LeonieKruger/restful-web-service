11bfdc4d61ca91a57c6debaed0e6d536d9f82cf8
LeonieKruger <leonie@andilesolutions.com>
2019-05-15 14:05:04 +1200

    added a new resource mapping

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,18 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message;
+}
